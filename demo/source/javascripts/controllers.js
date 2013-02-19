d08aad5909044eb36f229a0438c9de7674f8b465
Tyom Semonov <inspiro@gmail.com>
2013-02-19 00:57:53 +0000

    Hooked up options menu

--- demo/source/javascripts/controllers.js
+++ demo/source/javascripts/controllers.js
@@ -1,12 +1,51 @@
 angular.module('controllers', [])
-  .controller('testCase', function testCase($scope) {
-    $scope.options = [
-      {
-        name: "Gutterless",
-        instructions: {
+  .controller('testCase1', function testCase1($scope) {
+    $scope.options = {
+      gutterless: {
+        label: "Gutterless",
+        instructions: [{
           selector: ".l-row",
           modifier: "m-gutterless"
+        }]
+      }
+    };
+  })
+  .controller('testCase2', function testCase2($scope) {
+    $scope.options = {
+      gutterless: {
+        label: "Gutterless",
+        instructions: [{
+          selector: ".l-row",
+          modifier: "m-gutterless"
+        }]
+      },
+      offsets: {
+        group: {
+          offset1: {
+            label: "Offsets 1",
+            instructions: [
+              {
+                selector: ".l-col:first",
+                modifier: "m-pull-1"
+              }, {
+                selector: ".l-col:last",
+                modifier: "m-push-1"
+              }
+            ]
+          },
+          offset2: {
+            label: "Offsets 2",
+            instructions: [
+              {
+                selector: ".l-col:first",
+                modifier: "m-pull-2"
+              }, {
+                selector: ".l-col:last",
+                modifier: "m-push-2"
+              }
+            ]
+          }
         }
       }
-    ];
+    };
   });
--- demo/source/javascripts/directives.js
+++ demo/source/javascripts/directives.js
@@ -5,15 +5,55 @@ angular.module('directives', [])
       replace: true,
       transclude: true,
       scope: {
-        options: "=items"
+        defaults: "@",
+        options:  "=items"
       },
-      templateUrl: 'partials/options.html',
+      templateUrl: 'partials/options-menu.html',
       controller: function($scope, $element, $attrs) {
-        $scope.toggle = function(option) {
-          var root = angular.element($element).closest('.ng-scope');
-          root.find(option.instructions.selector).toggleClass(option.instructions.modifier);
+        var root = angular.element($element).closest('article');
 
+        // Toggle defaults
+        $attrs.$observe($scope.defaults, function() {
+          if($scope.defaults) {
+            $scope.defaults.split(' ').forEach(function(item) {
+              var path = item.split('.');
+              var obj;
+
+              if(path.length > 1) {
+                obj = $scope.options[path[0]]['group'][path[1]];
+              } else {
+                obj = $scope.options[item];
+              }
+
+              $scope.toggle(obj);
+            });
+          }
+        });
+
+        $scope.toggle = function(option, group) {
+          // Don't toggle group container
+          if(option.group) {
+            return;
+          }
+          if(group) {
+            angular.forEach(group, function(set, name) {
+              if(!option.active) {
+                untoggle(set);
+              }
+            });
+          }
+
+          option.instructions.forEach(function(instruction) {
+            root.find(instruction.selector).toggleClass(instruction.modifier);
+          });
           option.active = !option.active;
+        };
+
+        untoggle = function(option) {
+          option.instructions.forEach(function(instruction) {
+            root.find(instruction.selector).removeClass(instruction.modifier);
+          });
+          option.active = false;
         };
       }
     };
